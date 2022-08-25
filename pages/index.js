78b56d3de3519a31d2e01080798794cee2216fda
Brian Pompey <brianpompey@brians-mbp.mynetworksettings.com>
2022-08-24 20:33:36 -0400

    working on using react-table

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,58 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import React, { useMemo, useState, useEffect } from "react";
 import ConferenceTable from '../components/ConferenceTable'
 
 export default function Home() {
+
+  const columns = useMemo(
+    () => [
+      {
+        // first group - TV Show
+        Header: "TV Show",
+        // First group columns
+        columns: [
+          {
+            Header: "Name",
+            accessor: "show.name"
+          },
+          {
+            Header: "Type",
+            accessor: "show.type"
+          }
+        ]
+      },
+      {
+        // Second group - Details
+        Header: "Details",
+        // Second group columns
+        columns: [
+          {
+            Header: "Language",
+            accessor: "show.language"
+          },
+          {
+            Header: "Genre(s)",
+            accessor: "show.genres"
+          },
+          {
+            Header: "Runtime",
+            accessor: "show.runtime"
+          },
+          {
+            Header: "Status",
+            accessor: "show.status"
+          }
+        ]
+      }
+    ],
+    []
+  );
   return (
     <div>
       <h1>Hello World</h1>
-      <ConferenceTable/>
+      <ConferenceTable columns={columns} data={data} />
     </div>
   )
 }

2c30ec333ddaa8681cd50f849dbbf5deab9de401
Brian Pompey <brianpompey@brians-mbp.mynetworksettings.com>
2022-09-01 11:00:05 -0400

    adding a header component

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,12 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import React, { useMemo, useState, useEffect } from "react";
 import ConferenceTable from '../components/ConferenceTable'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
     <div>
+      <Header/>
       <h1 className="text-3xl font-bold underline">Hello World</h1>
       <ConferenceTable />
     </div>
